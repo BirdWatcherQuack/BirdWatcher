ffaaacf7d3b40aa11d174932fe42260e26768a91
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-28 12:32:33 -0400

    facebook login

--- public/js/login.js
+++ public/js/login.js
@@ -65,3 +65,51 @@ const signupFormHandler = async (event) => {
 document
   .querySelector('.sign-up-form')
   .addEventListener('submit', signupFormHandler);
+
+
+//sign up with facebook
+
+function statusChangeCallback(response) {
+  console.log('statusCnahgeCallback');
+  console.log(response);
+
+  if (response.status === 'connected') {
+    testAPI();
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '781387505856541',
+    cookie: true,
+    xfbml: true,
+    version: 'v10.0'
+  });
+
+  FB.AppEvents.logPageView();
+
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById('#facebook')) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// function checkLoginState() {
+//   FB.getLoginStatus(function (response) {
+//     statusChangeCallback(response);
+//   });
+// }
+
+// FB.getLoginStatus(function (response) {
+//   statusChangeCallback(response);
+// })
