ad8b41e9a8370c26b686555687d6913a7f9b0dc9
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-29 12:52:11 -0400

    front end fixed

---
+++ public/js/homepage.js
@@ -0,0 +1,8 @@
+
+//side bar
+function openSidebar() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+function closeSidebar() {
+  document.getElementById("mySidebar").style.display = "none";
+}
