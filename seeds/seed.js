368b3ec4159df542acacb39c9859a701ee775154
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-05-29 10:18:07 -0400

    seed.js overwrite

--- seeds/seed.js
+++ seeds/seed.js
@@ -14,32 +14,16 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-<<<<<<< HEAD
   await Bird.bulkCreate(birdData, {
     individualHooks: true,
     returning: true,
   });
   await Location.bulkCreate(locationData, {
-=======
-
-  process.exit(0);
-};
-
-seedDatabase();
-
-const seedBirdDatabase = async () => {
-  await sequelize.sync({ force: true });
-  await Bird.bulkCreate(birdData, {
->>>>>>> 4d022f672aa440f1ea5ed5d26df9097c840db598
     individualHooks: true,
     returning: true,
   });
   process.exit(0);
 };
 
-<<<<<<< HEAD
 
 seedDatabase();
-=======
-seedBirdDatabase();
->>>>>>> 4d022f672aa440f1ea5ed5d26df9097c840db598
