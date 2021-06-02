312f7dba6bb838e48c9515b521b506682e4e3217
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-06-01 22:03:38 -0400

    Models work. No user input errors. No seed errors.

--- models/index.js
+++ models/index.js
@@ -2,14 +2,30 @@ const User = require('./User');
 const Bird = require('./Bird');
 const Location = require('./Location');
 
-Bird.belongsToMany(User, {
-  //  foreignKey: 'user_id',
-  through: Location,
+// Bird.belongsToMany(User, {
+//   //  foreignKey: 'user_id',
+//   through: Location,
+// })
+
+// User.belongsToMany(Bird,{
+//   // foreignKey: 'bird_id',
+//   through: Location,
+// })
+
+User.hasMany(Location, {
+  foreignKey: 'user_id'
+});
+
+Bird.hasMany(Location, {
+  foreignKey: 'bird_id'
 })
 
-User.belongsToMany(Bird,{
-  // foreignKey: 'bird_id',
-  through: Location,
-}) 
+Location.belongsTo(Bird, {
+  foreignKey: 'bird_id'
+})
+
+Location.belongsTo(User, {
+  foreignKey: 'user_id'
+})
 
 module.exports = { User, Bird, Location };
