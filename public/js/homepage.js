87bd061a033888df6055d1de2ab985c4b9c08888
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-02 14:37:47 -0400

    config.json added, main handlebar fixed

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -6,12 +6,3 @@ function openSidebar() {
 function closeSidebar() {
   document.getElementById("mySidebar").style.display = "none";
 }
-
-// $('#viewAllB').click(function () {
-//   $('.allBirds').css('display', 'none');
-// })
-
-document.querySelectorAll('.allBirds').on('click', function () {
-  $('#viewAllB').hide();
-
-})
