121555c4554904494be194099bf32eff380c798c
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-06-03 21:46:50 -0400

    dependances deleted

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const birdRoutes = require('./bird-routes');
-const cardRoutes = require('./card-routes')
+
 
 router.use('/users', userRoutes);
 router.use('/birds', birdRoutes);
-router.use('/cards', cardRoutes)
+
 
 module.exports = router;
